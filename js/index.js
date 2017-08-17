3e2d9116598980c362dae88ec5d23fec24d3ff44
myName005 <eer37779@gmail.com>
2017-08-17 11:22:58 +0100

    add startPamador , startRest methods

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-
 var app = new Vue({
 	el:"#app",
 	components:{
@@ -7,9 +6,62 @@ var app = new Vue({
 	},
 	data:
 	{
-		pamadoroTime:20,
+		pamadoroTime:20,//in min
 		restTime:10,
 		numberOfSessions:5,
-		sessionsStarted:false
+		numberOfPassedSessions:0,
+		state:'beforeStart',
+
+	},
+	computed:{
+		isBeforStart(){
+			return this.state == 'beforeStart'
+		},
+		passedTime(){
+			return 0;
+		},
+		totalTime(){
+			if(this.state == 'pamadoro')
+				return this.passedTime;
+			return this.restTime;
+		}
+	},
+
+	methods:
+	{
+		startSessions()
+		{
+			this.startPamadero();
+		},
+		startPamadero()
+		{
+			var $this = this;
+			$this.state='pamadoro';
+
+			setTimeout(function () {
+				$this.numberOfPassedSessions++;
+
+				if($this.numberOfPassedSessions < $this.numberOfSessions)
+					$this.startRest();
+				else
+					$this.end()
+
+			}, $this.pamadoroTime*60000);
+		},
+		startRest()
+		{
+			var $this = this;
+			$this.state='rest';
+			setTimeout(function () {
+				$this.startPamadero();
+			}, $this.restTime*60000);
+		},
+		end()
+		{
+			this.state='end'
+		},
+		
+
 	}
+
 })
